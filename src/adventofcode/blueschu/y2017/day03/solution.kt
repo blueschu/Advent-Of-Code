aa085eb9eafe014e13fc1842943bed0a4d0d77d1
Brian Schubert <brianm.schubert@gmail.com>
2017-12-04 18:02:56 -0500

    Add solution for 2017 Day 3 Part 2

--- src/adventofcode/blueschu/y2017/day03/solution.kt
+++ src/adventofcode/blueschu/y2017/day03/solution.kt
@@ -10,6 +10,9 @@ fun main(args: Array<String>) {
     assertEquals(2, part1(23))
     assertEquals(31, part1(1024))
     println("Part 1: ${part1(input())}")
+
+    assertEquals(23, part2(20))
+    println("Part 2: ${part2(input())}")
 }
 
 fun part1(input: Int): Int {
@@ -36,4 +39,93 @@ fun part1(input: Int): Int {
         Math.abs(distanceAroundRing % (ringIndex - 1) - perpendicularDistance)
 
     return perpendicularDistance + offsetDistance
+}
+
+
+enum class Direction {
+    NORTH, EAST, SOUTH, WEST;
+
+    fun turnedLeft(): Direction =
+        if (this == NORTH) WEST else values()[ordinal - 1]
+
+    fun turnedRight(): Direction = values()[(ordinal + 1) % values().size]
+}
+
+// for part 2, very messy but functional
+class CellGrid(radius: Int) {
+
+    private inner class Walker(var xPos: Int, var yPos: Int) {
+        var facing: Direction = Direction.EAST
+
+        fun advance(): Int {
+            // advance one cell
+            when (facing) {
+                Direction.NORTH -> yPos++
+                Direction.EAST -> xPos++
+                Direction.SOUTH -> yPos--
+                Direction.WEST -> xPos--
+
+            }
+            // check if cell to the left is open, if so turn
+            if (0 == when (facing) {
+                Direction.NORTH -> this@CellGrid[xPos - 1, yPos]
+                Direction.EAST -> this@CellGrid[xPos, yPos + 1]
+                Direction.SOUTH -> this@CellGrid[xPos + 1, yPos]
+                Direction.WEST -> this@CellGrid[xPos, yPos - 1]
+            }) facing = facing.turnedLeft()
+
+            return loadCurrentCell()
+        }
+
+        private fun loadCurrentCell(): Int {
+            val cellValue = this@CellGrid[xPos + 1, yPos] +
+                this@CellGrid[xPos + 1, yPos + 1] +
+                this@CellGrid[xPos, yPos + 1] +
+                this@CellGrid[xPos - 1, yPos + 1] +
+                this@CellGrid[xPos - 1, yPos] +
+                this@CellGrid[xPos - 1, yPos - 1] +
+                this@CellGrid[xPos, yPos - 1] +
+                this@CellGrid[xPos + 1, yPos - 1]
+            this@CellGrid[xPos, yPos] = cellValue
+            return cellValue
+        }
+    }
+
+    // walker starts at the origin
+    private val gridWalker: Walker = Walker(radius + 1, radius + 1)
+
+    private val width: Int = (radius * 2) + 1
+
+    private val grid = Array<Int>(width * width, { _ -> 0 })
+
+    private operator fun get(i: Int, j: Int) = try {
+        grid[i * width + j]
+    } catch (e: IndexOutOfBoundsException) {
+        0
+    }
+
+    private operator fun set(i: Int, j: Int, value: Int) {
+        // println("Adding $value @ [$i, $j]")
+        grid[i * width + j] = value
+
+    }
+
+    fun nextCell(): Int = gridWalker.advance()
+
+    init {
+        this[radius + 1, radius + 1] = 1
+    }
+}
+
+fun cellGridFor(cellNumber: Int) =
+    CellGrid((Math.ceil(Math.sqrt(cellNumber.toDouble())) / 2).toInt())
+
+fun part2(input: Int): Int {
+    val cellGrid = cellGridFor(input)
+    while (true) {
+        val cell = cellGrid.nextCell()
+        if (cell > input) {
+            return cell
+        }
+    }
 }
