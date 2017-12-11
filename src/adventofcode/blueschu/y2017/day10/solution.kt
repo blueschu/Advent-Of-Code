9a107656f528723ec48a3968bb778fca34672bc4
Brian Schubert <brianm.schubert@gmail.com>
2017-12-10 21:59:52 -0500

    Add solution for 2017 Day 10 Part 2

--- src/adventofcode/blueschu/y2017/day10/solution.kt
+++ src/adventofcode/blueschu/y2017/day10/solution.kt
@@ -13,6 +13,13 @@ val input: String by lazy {
 fun main(args: Array<String>) {
     assertEquals(12, part1("3,4,1,5", Knot(circleSize = 5)))
     println("Part 1: ${part1(input)}")
+
+    assertEquals("a2582a3a0e66e6e86e3812dcb672a272", knotHash(""))
+    assertEquals("33efeb34ea91902bb2f59c9920caa6cd", knotHash("AoC 2017"))
+    assertEquals("3efbe78a8d82f29979031a4aa0b16a9d", knotHash("1,2,3"))
+    assertEquals("63960835bcdc130f0b66d7ff4f6a5a8e", knotHash("1,2,4"))
+    println("Part 2: ${knotHash(input)}")
+
 }
 
 fun part1(lengthString: String, knot: Knot = Knot()): Int {
@@ -78,5 +85,25 @@ fun <T> Array<T>.rotate(shift: Int) {
         reverseRange(0, -wrappedShift - 1)
         reverseRange(-wrappedShift, size - 1)
         reverse()
+    }
+}
+
+// part 2
+fun knotHash(stringToBeHashed: String): String {
+    fun Knot.twistLength(l: Byte) = twistLength(l.toInt())
+    val hashInput = byteArrayOf(*stringToBeHashed.toByteArray(), 17, 31, 73, 47, 23)
+    val knot = Knot()
+    repeat(times = 64) {
+        hashInput.forEach { knot.twistLength(it) }
+    }
+    val sparseHash = knot.circleMarks
+    val denseHash = (0 until 16).map {
+        sparseHash
+            .copyOfRange(it * 16,  it * 16 + 16)
+            .reduce { acc, next -> acc xor next }
+    }
+    return denseHash.fold("") { hash, next: Int ->
+        val hex = next.toString(radix = 0x10) // hexadecimal string
+        hash + if (hex.length == 1) "0" + hex else hex // pad with leading 0
     }
 }
